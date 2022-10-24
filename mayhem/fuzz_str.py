73c3ecbea98ad34b9821742fe5ca8d536b6599b8
McKenna Dallmeyer <dallmeyerm@att.net>
2022-10-23 21:11:06 -0700

    Changed state

--- mayhem/fuzz_str.py
+++ mayhem/fuzz_str.py
@@ -4,13 +4,16 @@ import atheris
 import sys
 
 with atheris.instrument_imports():
-    from docstring_parser import parse, compose
-
+    from docstring_parser import rest, parser
 
 def TestOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
-    doc = parse(fdp.ConsumeUnicodeNoSurrogates(128))
-    compose(doc)
+
+    doc = parser.parse(fdp.ConsumeUnicodeNoSurrogates(128))
+    parser.compose(doc)
+
+    doc2 = rest.parse(fdp.ConsumeUnicodeNoSurrogates(128))
+    rest.compose(doc2)
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
